2e5cfd3aaec1b7fa743c9f6d5993da49c1d85dee
jun <junaidvayoli@gmail.com>
2014-10-11 17:54:41 +0530

    Adding the Service class

---
+++ app/src/main/java/smartauto/betaturtle/in/smartauto/MyService.java
@@ -0,0 +1,36 @@
+package smartauto.betaturtle.in.smartauto;
+
+/**
+ * Created by jun on 11/10/14.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, " MyService Created ", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Toast.makeText(this, " MyService Started", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, "Servics Stopped", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
+}
