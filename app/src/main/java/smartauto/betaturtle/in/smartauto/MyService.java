cd2f221960dd3aed5bbe1a1fcdf01c2fc5999832
jun <junaidvayoli@gmail.com>
2014-10-15 14:17:49 +0530

    Location accessing and posting

--- app/src/main/java/smartauto/betaturtle/in/smartauto/MyService.java
+++ app/src/main/java/smartauto/betaturtle/in/smartauto/MyService.java
@@ -2,11 +2,17 @@ package smartauto.betaturtle.in.smartauto;
 
 /**
  * Created by jun on 11/10/14.
+ *
  */
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MyService extends Service {
 
@@ -24,8 +30,28 @@ public class MyService extends Service {
 
     @Override
     public void onStart(Intent intent, int startId) {
-        Toast.makeText(this, " MyService Started. Now "+ count, Toast.LENGTH_LONG).show();
         count++;
+        final String android_id = Settings.Secure.getString(this.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        Toast.makeText(this, android_id+" MyService Started. Now "+ count, Toast.LENGTH_SHORT).show();
+        new MyLocation().getLocation(getApplicationContext(), new MyLocation.LocationResult(){
+            @Override
+            public void gotLocation (Location location) {
+
+                JSONObject json = new JSONObject();
+
+                try {
+                    json.put("deviceid",android_id);
+                    json.put("lat", location.getLatitude());
+                    json.put("lng", location.getLongitude());
+                    json.put("alt", location.getAltitude());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                new AsyncPost("http://better-brett.gopagoda.com/location", json).execute();
+            }
+        });
+
     }
 
     @Override
